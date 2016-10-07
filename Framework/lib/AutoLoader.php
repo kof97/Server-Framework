66604538b44ec3935e0ec302c1ff64ac9b72190a
kof97 <1048434786@qq.com>
2016-10-07 21:59:03 +0800

    初始化

--- Framework/lib/AutoLoader.php
+++ Framework/lib/AutoLoader.php
@@ -36,7 +36,7 @@ class AutoLoader
 }
 
 if (version_compare(PHP_VERSION, '5.4.0', '<')) {
-    // throw new Exception('This Framework requires PHP version 5.4 or higher.');
+    throw new Exception('This Framework requires PHP version 5.4 or higher.');
 }
 
 define('DS', DIRECTORY_SEPARATOR);
