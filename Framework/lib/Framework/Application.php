51a24adcfcc9244ff00d41ac552450bca7c50d09
kof97 <1048434786@qq.com>
2016-10-08 10:39:10 +0800

    init

---
+++ Framework/lib/Framework/Application.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Class Application.
+ *
+ * @category PHP
+ */
+class Application
+{
+    
+    private function __construct()
+    {
+        // It should never be used.
+    }
+
+    public static function init()
+    {
+        
+    }
+}
+
+// end of script
--- Framework/web/index.php
+++ Framework/web/index.php
@@ -1,5 +1,11 @@
 <?php
 
+if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+    throw new Exception('This Framework requires PHP version 5.4 or higher.');
+}
+
+define('DS', DIRECTORY_SEPARATOR);
+
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'AutoLoader.php';
 
 // end of script
