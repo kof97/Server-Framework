8680274238ff4e067b3358eace64933a285dee25
kof97 <1048434786@qq.com>
2016-10-08 12:39:53 +0800

    init

--- Framework/lib/Framework/Application.php
+++ Framework/lib/Framework/Application.php
@@ -1,4 +1,7 @@
 <?php
+
+namespace Framework;
+
 /**
  * Class Application.
  *
@@ -12,9 +15,35 @@ class Application
         // It should never be used.
     }
 
-    public static function init()
+    public static function init($conf)
     {
-        
+        $conf = parse_ini_file($conf, true);
+
+        $mode = isset($conf['base']['mode']) ? $conf['base']['mode'] : 'restful';
+
+        if (!isset($conf[$mode])) {
+            throw new Exception("Not found the mode ['{$mode}']");
+        }
+
+        $conf = $conf[$mode];
+
+        switch ($mode) {
+            case 'restful':
+                self::initRestful($conf);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private static function initRestful($conf)
+    {
+        $resource_root = ROOT . isset($conf['resource_root']) ? $conf['resource_root'] : 'restful/Resource';
+        $model_root = ROOT . isset($conf['model_root']) ? $conf['model_root'] : 'restful/Model';
+
+        is_dir($resource_root) || self::makeDir($resource_root);
+        is_dir($model_root) || self::makeDir($model_root);
     }
 }
 
--- Framework/web/index.php
+++ Framework/web/index.php
@@ -1,11 +1,21 @@
 <?php
 
 if (version_compare(PHP_VERSION, '5.4.0', '<')) {
-    throw new Exception('This Framework requires PHP version 5.4 or higher.');
+    // throw new Exception('This Framework requires PHP version 5.4 or higher.');
 }
 
 define('DS', DIRECTORY_SEPARATOR);
 
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'AutoLoader.php';
+define('ROOT', dirname(__DIR__) . DS);
+define('BIN_PATH', ROOT . 'bin' . DS);
+
+
+$conf = ROOT . 'conf' . DS . 'server.ini';
+
+var_dump(ROOT);
+
+require ROOT . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'AutoLoader.php';
+
+Framework\Application::init($conf);
 
 // end of script
