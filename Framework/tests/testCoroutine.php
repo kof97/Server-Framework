e0f98c854d730057162f296504b136f2ac55c752
kof97 <1048434786@qq.com>
2016-10-12 10:05:20 +0800

    test Coroutine

--- Framework/tests/testCoroutine.php
+++ Framework/tests/testCoroutine.php
@@ -9,7 +9,6 @@ use Framework\Coroutine\Task;
 function getTaskId() {
     return new SystemCall(
         function(Task $task, Scheduler $scheduler) {
-
             $task->setSendValue($task->getTaskId());
             $scheduler->schedule($task);
         }
