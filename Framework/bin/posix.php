88b8535181a7a6f155f93f7a0f562d13347ad3d6
kof97 <1048434786@qq.com>
2016-11-06 12:32:22 +0800

    update

--- Framework/bin/posix.php
+++ Framework/bin/posix.php
@@ -4,7 +4,6 @@ $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 
 require $root . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'AutoLoader.php';
 
-
 $test = new Framework\Shell\Daemon();
 
 $test->init($argc, $argv);
--- Framework/src/Framework/Shell/Daemon.php
+++ Framework/src/Framework/Shell/Daemon.php
@@ -11,145 +11,145 @@ use Framework\Shell\Signal;
  */
 class Daemon
 {
-	protected $pidfile;
+    protected $pidfile;
 
-	public function __construct()
-	{
-		$this->pidfile = './run/' . __CLASS__ . '.pid';
+    public function __construct()
+    {
+        $this->pidfile = '../run/' . __CLASS__ . '.pid';
 
-		$this->signal();
-	}
+        $this->signal();
+    }
 
-	private function signal()
-	{
-		pcntl_signal(SIGHUP, function ($signo) {
-			echo 'The server has been reload' . PHP_EOL;
-			Signal::set($signo);
-		});
-	}
+    private function signal()
+    {
+        pcntl_signal(SIGHUP, function ($signo) {
+            echo 'The server has been reload' . PHP_EOL;
+            Signal::set($signo);
+        });
+    }
 
-	private function daemon()
-	{
-		if (is_file($this->pidfile)) {
-			echo "The file $this->pidfile exists" . PHP_EOL;
-			echo 'It has already started ! !' . PHP_EOL;
-			exit;
-		}
+    private function daemon()
+    {
+        if (is_file($this->pidfile)) {
+            echo "The file $this->pidfile exists" . PHP_EOL;
+            echo 'It has already started ! !' . PHP_EOL;
+            exit;
+        }
 
-		$pid = pcntl_fork();
-		if ($pid == -1) {
-			die('Could not fork');
-		} else if ($pid) {
-			// pcntl_wait($status);
-			exit;
-		} else {
-			file_put_contents($this->pidfile, getmypid());
+        $pid = pcntl_fork();
+        if ($pid == -1) {
+            die('Could not fork');
+        } else if ($pid) {
+            // pcntl_wait($status);
+            exit;
+        } else {
+            file_put_contents($this->pidfile, getmypid());
 
-			return getmypid();
-		}
-	}
+            return getmypid();
+        }
+    }
 
-	private function run()
-	{
-		while (true) {
-			pcntl_signal_dispatch();
+    private function run()
+    {
+        while (true) {
+            pcntl_signal_dispatch();
 
-			sleep(2);
+            sleep(2);
 
-			if (Signal::get() == SIGHUP) {
-				Signal::reset();
-				break;
-			}
-		}
-	}
+            if (Signal::get() == SIGHUP) {
+                Signal::reset();
+                break;
+            }
+        }
+    }
 
-	private function start()
-	{
-		$pid = $this->daemon();
+    private function start()
+    {
+        $pid = $this->daemon();
 
-		echo 'Start the server' . PHP_EOL;
+        echo 'Start the server' . PHP_EOL;
 
-		while (true) {
-			sleep(1);
+        while (true) {
+            sleep(1);
 
-			$this->run();
-		}
-	}
+            $this->run();
+        }
+    }
 
-	private function stop()
-	{
-		if (is_file($this->pidfile)) {
-			$pid = file_get_contents($this->pidfile);
-			unlink($this->pidfile);
+    private function stop()
+    {
+        if (is_file($this->pidfile)) {
+            $pid = file_get_contents($this->pidfile);
+            unlink($this->pidfile);
 
-			echo 'Stop the server' . PHP_EOL;
+            echo 'Stop the server' . PHP_EOL;
 
-			posix_kill($pid, 9);
-		} else {
-			echo 'Not found the server ! !' . PHP_EOL;
-		}
-	}
+            posix_kill($pid, 9);
+        } else {
+            echo 'Not found the server ! !' . PHP_EOL;
+        }
+    }
 
-	private function reload()
-	{
-		if (!is_file($this->pidfile)) {
-			echo 'Please start the server first' . PHP_EOL;
-			exit;
-		}
+    private function reload()
+    {
+        if (!is_file($this->pidfile)) {
+            echo 'Please start the server first' . PHP_EOL;
+            exit;
+        }
 
-		$pid = file_get_contents($this->pidfile);
-		posix_kill($pid, SIGHUP);
-	}
+        $pid = file_get_contents($this->pidfile);
+        posix_kill($pid, SIGHUP);
+    }
 
-	private function restart()
-	{
-		if (is_file($this->pidfile)) {
-			$this->stop();
-		}
+    private function restart()
+    {
+        if (is_file($this->pidfile)) {
+            $this->stop();
+        }
 
-		$this->start();
-	}
+        $this->start();
+    }
 
-	private function help()
-	{
-		echo 'Usage:' . PHP_EOL;
-		echo '- start | stop | restart | reload | help' . PHP_EOL;
-		exit;
-	}
+    private function help()
+    {
+        echo 'Usage:' . PHP_EOL;
+        echo '- start | stop | restart | reload | help' . PHP_EOL;
+        exit;
+    }
 
-	public function init($argc, $argv)
-	{
-		if ($argc < 2) {
-			echo 'Please input params' . PHP_EOL;
-			exit;
-		}
+    public function init($argc, $argv)
+    {
+        if ($argc < 2) {
+            echo 'Please input params' . PHP_EOL;
+            exit;
+        }
 
-		$cmd = $argv[1];
-		switch ($cmd) {
-			case 'start':
-				$this->start();
-				break;
+        $cmd = $argv[1];
+        switch ($cmd) {
+            case 'start':
+                $this->start();
+                break;
 
-			case 'stop':
-				$this->stop();
-				break;
+            case 'stop':
+                $this->stop();
+                break;
 
-			case 'reload':
-				$this->reload();
-				break;
+            case 'reload':
+                $this->reload();
+                break;
 
-			case 'restart':
-				$this->restart();
-				break;
+            case 'restart':
+                $this->restart();
+                break;
 
-			case 'help':
-				$this->help();
-				break;
+            case 'help':
+                $this->help();
+                break;
 
-			default:
-				break;
-		}
-	}
+            default:
+                break;
+        }
+    }
 }
 
 // end of script
