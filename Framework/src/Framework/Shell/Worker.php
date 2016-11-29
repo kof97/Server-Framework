76a83312cf86a34b637a2781e10997181d353341
kof97 <1048434786@qq.com>
2016-11-29 17:45:43 +0800

    add stream

--- Framework/src/Framework/Shell/Worker.php
+++ Framework/src/Framework/Shell/Worker.php
@@ -21,6 +21,8 @@ class Worker
 
     protected $socketName = '';
 
+    protected $context = null;
+
     public function __construct($socket_name = '')
     {
         $info = parse_url($socket_name);
@@ -31,6 +33,11 @@ class Worker
         isset($info['host']) && $this->host = $info['host'];
         isset($info['port']) && $this->port = $info['port'];
         isset($info['path']) && $this->endpoint = $info['path'];
+
+        if ($socket_name !== '') {
+            $context_option['socket']['backlog'] = 1024;
+            $this->context = stream_context_create($context_option);
+        }
     }
 
     public function run()
@@ -46,12 +53,23 @@ class Worker
     protected function listen()
     {
 
+        $flags  = $this->scheme === 'udp' ? STREAM_SERVER_BIND : STREAM_SERVER_BIND | STREAM_SERVER_LISTEN;
+        $errno  = 0;
+        $errmsg = '';
 
+        $this->socket = stream_socket_server($this->socketName, $errno, $errmsg, $flags, $this->context);
 
+        // Try to open keepalive for tcp and disable Nagle algorithm.
+        if (function_exists('socket_import_stream') && $this->scheme === 'tcp') {
+            $socket = socket_import_stream($this->socket);
+            @socket_set_option($socket, SOL_SOCKET, SO_KEEPALIVE, 1);
+            @socket_set_option($socket, SOL_TCP, TCP_NODELAY, 1);
+        }
 
+        stream_set_blocking($this->socket, 0);
 
         if (Master::$globalEvent) {
-            if (Master::$protocol === 'udp') {
+            if ($this->scheme === 'udp') {
                 Master::$globalEvent->add($this->socket, EventInterface::EV_READ, array($this, 'acceptUdpConnection'));
             } else {
                 Master::$globalEvent->add($this->socket, EventInterface::EV_READ, array($this, 'acceptConnection'));
