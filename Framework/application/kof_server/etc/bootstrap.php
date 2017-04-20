256de0adc2260ac2bbf00096b38cc02dae3b0189
kof97 <1048434786@qq.com>
2017-04-20 17:23:23 +0800

    删除测试application

--- Framework/application/kof_server/etc/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require dirname(dirname(dirname(__DIR__))) . DIRECTORY_SEPARATOR . 'etc' . DIRECTORY_SEPARATOR . 'bootstrap.php';
-
-require ROOT . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'loader.php';
-
-
-// end of script
--- Framework/application/kof_server/web/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'etc/bootstrap.php';
-
-Framework\Application::run(dirname(__DIR__) . DIRECTORY_SEPARATOR);
-
-// end of script
