cdbe3ed093ff812e2c4e3f1ba979cf527008ddb2
Arno <1048434786@qq.com>
2017-04-20 19:06:47 +0800

    init

--- Framework/bin/init.php
+++ Framework/bin/init.php
@@ -7,6 +7,6 @@ if (version_compare(PHP_VERSION, '5.4.0', '<')) {
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'etc/bootstrap.php';
 require ROOT . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'loader.php';
 
-Framework\Shell\Cmd::init();
+Server\Shell\Cmd::init();
 
 // end of script
--- Framework/src/Server/Shell/Cmd.php
+++ Framework/src/Server/Shell/Cmd.php
@@ -3,7 +3,7 @@
 namespace Server\Shell;
 
 use \Exception;
-use Server\Common\File;
+use Framework\Common\File;
 
 /**
  * Class Cmd.
@@ -31,40 +31,6 @@ class Cmd
 
 		is_dir($run) || File::makeDir($run);
 		is_dir($log) || File::makeDir($log);
-
-		if (!isset($conf[$mode])) {
-			throw new Exception("Not found the mode ['{$mode}']");
-		}
-
-		$conf = $conf[$mode];
-
-		switch ($mode) {
-			case 'restful':
-				self::initRestful($conf, $root);
-				break;
-
-			default:
-				break;
-		}
-	}
-
-	private static function initRestful($conf, $root)
-	{
-		$restful_root = $root . 'restful' . DIRECTORY_SEPARATOR;
-
-		is_dir($restful_root) || File::makeDir($restful_root);
-
-		$resource_root = $restful_root . 'Resource';
-		$model_root = $restful_root . 'Model';
-
-		is_dir($resource_root) || File::makeDir($resource_root);
-		is_dir($model_root) || File::makeDir($model_root);
-
-		chmod($restful_root, 0644);
-		chmod($resource_root, 0644);
-		chmod($model_root, 0644);
-
-		echo PHP_EOL . 'Init restful OK' . PHP_EOL;
 	}
 }
 
