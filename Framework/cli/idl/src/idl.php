135751f5eae9849eb1cc3dcf8f5f08d7cf5e0d56
kof97 <1048434786@qq.com>
2017-07-09 19:51:56 +0800

    idl validater

--- Framework/cli/idl/src/idl.php
+++ Framework/cli/idl/src/idl.php
@@ -3,9 +3,11 @@
 require dirname(dirname(dirname(__DIR__))) . DIRECTORY_SEPARATOR . 'etc' . DIRECTORY_SEPARATOR . 'bootstrap.php';
 require ROOT . 'src' . DIRECTORY_SEPARATOR . 'loader.php';
 
+$app = 'demo';
+
 $config = array(
-	'idl' => ROOT . '/application/demo/etc/idl',
-	'idl_config' => ROOT . '/application/demo/inc/IDL',
+	'idl' => ROOT . '/application/' . $app . '/etc/idl',
+	'idl_config' => ROOT . '/application/' . $app . '/inc/IDL',
 );
 
 new IDL\Generator\Monitor($config);
